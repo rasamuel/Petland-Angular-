219205d34db373709c48f227c49b566920f090db
juan.duarte017@hotmail.com <juan.duarte017@hotmail.com>
2024-09-17 10:43:27 -0500

    Update

--- PetLand/src/app/services/pet.service.ts
+++ PetLand/src/app/services/pet.service.ts
@@ -7,8 +7,17 @@ import { Pet } from '../models/pet.model';
 export class PetService {
   private pets: Pet[] = [
     { id: 1, nombre: 'Fido', raza: 'Labrador', edad: 5, peso: 20, enfermedad: 'None', estado: true, imageUrl: 'url1' },
-    { id: 2, nombre: 'Max', raza: 'Bulldog', edad: 3, peso: 15, enfermedad: 'Artritis', estado: false, imageUrl: 'url2' }
+    { id: 2, nombre: 'Max', raza: 'Bulldog', edad: 3, peso: 15, enfermedad: 'Artritis', estado: false, imageUrl: 'url2' },
+    { id: 3, nombre: 'Luna', raza: 'Beagle', edad: 4, peso: 12, enfermedad: 'Alergias', estado: true, imageUrl: 'url3' },
+    { id: 4, nombre: 'Rocky', raza: 'Pastor Alemán', edad: 6, peso: 30, enfermedad: 'Displasia de cadera', estado: false, imageUrl: 'url4' },
+    { id: 5, nombre: 'Bella', raza: 'Poodle', edad: 2, peso: 8, enfermedad: 'Ninguna', estado: true, imageUrl: 'url5' },
+    { id: 6, nombre: 'Charlie', raza: 'Golden Retriever', edad: 7, peso: 32, enfermedad: 'Artritis', estado: false, imageUrl: 'url6' },
+    { id: 7, nombre: 'Simba', raza: 'Chihuahua', edad: 1, peso: 3, enfermedad: 'Ninguna', estado: true, imageUrl: 'url7' },
+    { id: 8, nombre: 'Milo', raza: 'Boxer', edad: 5, peso: 28, enfermedad: 'Epilepsia', estado: true, imageUrl: 'url8' },
+    { id: 9, nombre: 'Toby', raza: 'Rottweiler', edad: 6, peso: 40, enfermedad: 'Problemas cardíacos', estado: false, imageUrl: 'url9' },
+    { id: 10, nombre: 'Nala', raza: 'Husky Siberiano', edad: 4, peso: 25, enfermedad: 'Ninguna', estado: true, imageUrl: 'url10' }
   ];
+  
 
   getPets(): Pet[] {
     return this.pets;
